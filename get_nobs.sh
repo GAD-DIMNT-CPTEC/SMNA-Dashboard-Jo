9204043d7716543149664a613e208bbb46260e82
Carlos Frederico Bastarz <cfbastarz@gmail.com>
2023-09-26 09:41:46 -0300

    Atualizando data do dashboard para 2023092500 e corigindo comando ls para obter o arquivo de log mais recente no script get_nobs.sh

---
+++ get_nobs.sh
@@ -0,0 +1,44 @@
+#! /bin/bash
+
+# Script para obter e organizar as informações dos logs do GSI
+# para dois experimentos.
+
+# @cfbastarz (31/08/2023)
+
+#inctime=/cray_home/carlos_bastarz/bin/inctime
+inctime=/opt/inctime/bin/inctime
+
+Exps=(JGerd preOper)
+
+datai=2023010100
+dataf=2023092500
+
+data=${datai}
+
+while [ ${data} -le ${dataf} ]
+do
+
+  for exp in ${Exps[@]}
+  do
+
+    echo ${data} ${exp}
+
+    lpath=/home/carlos/GitHub/SMNA-Dashboard-Jo/${exp}
+    rpath=/extra2/XC50_SMNA_GSI_dataout_${exp}
+
+    mkdir -p ${lpath}/${data}
+
+    #logf=$(ls ${rpath}/${data}/gsiStdout_${data}.runTime-*.log | tail -1)
+    logf=$(ls -t1 ${rpath}/${data}/gsiStdout_${data}.runTime-*.log | head -1)
+  
+    mkdir -p ${lpath}/${data}
+  
+    cp -v ${logf} ${lpath}/${data}/gsiStdout_${data}.log
+
+  done
+
+  data=$(${inctime} ${data} +6hr %y4%m2%d2%h2)
+
+done
+
+exit 0
